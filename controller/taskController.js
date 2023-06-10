cc93970f091afd1bc3f1ad2a35e93508f0909819
unknown <abdelrheem2204@gmail.com>
2023-06-10 05:51:03 +0200

    stil in front-end

--- controller/taskController.js
+++ controller/taskController.js
@@ -4,8 +4,11 @@ const get_tasks = (req, res) => {
 
 const post_task = (req, res) => {
     taskName = req.body.task;
+    console.log(taskName);
+    res.send({task: taskName, redirect: 'index'});
 }
 
 module.exports = {
     get_tasks,
+    post_task
 }
---
+++ public/main.js
@@ -0,0 +1,21 @@
+const getTask = document.querySelector('form');
+
+getTask.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const task = getTask.task.value;
+
+    try{
+        const res = fetch('../add-task', {
+            method: 'POST',
+            body: JSON.stringify({taskName: task}),
+            headers: {'Content-Type': 'application/json'}
+        })
+
+        const data = await res.json();
+        console.log(data);
+
+    } catch(err) {
+        console.log(err);
+    }
+});
+
